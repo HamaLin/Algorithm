3aeb17b23b9bd5c7e651fa11603289b5a431c4d7
HamaLin <86543050+HamaLin@users.noreply.github.com>
2023-04-27 15:42:22 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 120 ms, Memory: 9648 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -5,11 +5,18 @@ let input = require("fs")
 
 function starPyramid(num) {
   let result = "";
+  let count = 0;
 
-  for (let i = 1; i <= num; i++) {
-    for (let j = 0; j < i; j++) result += "*";
+  for (let i = 0; i < num * num; i++) {
+    if (Number.isInteger(i / num)) {
+      result += i > 0 ? "\n" : "";
+      count = i / num + 1;
+    }
 
-    result += "\n";
+    if (count) {
+      result += "*";
+      count--;
+    }
   }
 
   return result;
