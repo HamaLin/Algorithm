1a61109882e43195c363ec38f28f22e6b4fb5c0e
HamaLin <86543050+HamaLin@users.noreply.github.com>
2023-04-28 15:09:51 +0900

    [Bronze III] Title: 공 넣기, Time: 136 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.js
+++ 백준/Bronze/10810. 공 넣기/공 넣기.js
@@ -4,27 +4,17 @@ let input = require("fs")
   .trim()
   .split("\n");
 
-function createBucketObj(bucAmount) {
-  let result = {};
-  for (let i = 1; i <= +bucAmount; i++) {
-    result[`${i}`] = 0;
-  }
+// 구조 분해 이용
+function getBucketBallNumber(arr) {
+  const [nm, ...newArr] = arr;
+  let buckets = new Array(+nm.split(" ")[0]).fill(0);
 
-  return result;
+  newArr.forEach((element) => {
+    const [start, end, number] = element.split(" ");
+    buckets.fill(+number, +start - 1, +end);
+  });
+
+  return Object.values(buckets).toString().replaceAll(",", " ");
 }
 
-function getBucketBallNumber(bucAmount, arr) {
-  const bucket = createBucketObj(bucAmount);
-  arr.splice(0, 1);
-
-  for (let count = 0; count < arr.length; count++) {
-    let exe = arr[count].split(" ");
-    for (let i = +exe[0]; i <= +exe[1]; i++) {
-      bucket[`${i}`] = +exe[2];
-    }
-  }
-
-  return Object.values(bucket).toString().replaceAll(",", " ");
-}
-
-console.log(getBucketBallNumber(input[0].split(" ")[0], input));
+console.log(getBucketBallNumber(input));
