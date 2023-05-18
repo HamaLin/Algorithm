d0b25fc5e4fad655f30bc444defb3c967b1fa4a5
HamaLin <86543050+HamaLin@users.noreply.github.com>
2023-05-18 21:28:41 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 116 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.js
@@ -0,0 +1,11 @@
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ");
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+const V = Number(input[2]);
+
+console.log(Math.ceil((V - A) / (A - B) + 1));
